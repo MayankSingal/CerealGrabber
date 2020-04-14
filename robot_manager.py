6b45ae52d27dc7f25b36b5554700120d2fb3ccc6
MayankSingal <mayanksi@andrew.cmu.edu>
2020-04-14 19:27:42 -0400

    changes

--- robot_manager.py
+++ robot_manager.py
@@ -12,6 +12,8 @@ from pyrep.const import ConfigurationPathAlgorithms as Algos
 import pprint
 import time
 
+import copy
+
 from utils import RandomAgent, NoisyObjectPoseSensor, VisionObjectPoseSensor, RobotController
 
 
@@ -37,34 +39,109 @@ if __name__ == "__main__":
     
     ### Useful variables
     gripper_vertical_orientation = np.array([3.13767052e+00, 1.88300957e-03, 9.35417891e-01])
+    reset_position = env._robot.arm.get_tip().get_position()
+    reset_orientation = env._robot.arm.get_tip().get_orientation()
+    
+    mustard_orientation = [ 7.07037210e-01,  7.07173109e-01, -6.37740828e-04, -2.06269184e-03]
+    mustard_position = [ 0.31035879, -0.12106754,  0.90185165]
+
     
     
     
     ######################################### Motion Planning ##########################################################
     descriptions, obs = task.reset()
+    
 
 
     ## Motion 1
     ## (1) Sense mustard_grasp_point location. (2) Move gripper to a point 0.1m over mustard_grasp_point, while making it remain vertical.
     
     #(1)
-    next_position, next_orientation = robot_controller.get_pose_from_simulation("mustard_grasp_point")
+    next_position, next_orientation, mustard_obj = robot_controller.get_pose_and_object_from_simulation("mustard_grasp_point")
     
     #(2)
     next_position[2] += 0.1
     next_orientation = gripper_vertical_orientation
     robot_controller.move(next_position, next_orientation)
-    
+        
     ## Motion 2
     ## (1) Move downwards. (2) Close gripper
     
     #(1)
-    next_position[2] -= 0.1
-    next_orientation = gripper_vertical_orientation
-    robot_controller.move(next_position, next_orientation)
+    robot_controller.translate(z=-0.1)
     
     #(2)
     robot_controller.actuate_gripper(0)
+    
+
+    ## Motion 3
+    # (1) Move upwards. (2) Move near mustard reset position
+    
+    # (1)
+    robot_controller.translate(z=0.25, gripper_state=0, ignore_collisions=True)
+    
+    # (2)
+    req_position = copy.copy(mustard_position)
+    req_position[2] += 0.3
+    req_position[0] -= 0.3
+    robot_controller.move(req_position, gripper_vertical_orientation, gripper_state=0)
+    
+    
+    ## Motion 4
+    # (1) Rotate grasped object to vertical orientation.
+    
+    required_rotation = None   
+    mustard_quat = R.from_quat(list(mustard_obj.get_quaternion()))
+    desired_mustard_quat = R.from_quat(list(mustard_orientation))
+    required_rotation = mustard_quat*desired_mustard_quat.inv()
+    
+    end_effector_orintation = R.from_euler('xyz', env._robot.arm.get_tip().get_orientation())
+    new_orientation = end_effector_orintation * required_rotation
+    new_orientation = list(new_orientation.as_euler('xyz'))
+    
+    robot_controller.rotate_to(new_orientation, gripper_state=0, ignore_collisions=True)
+    
+  
+    ## Motion 5
+    # (1) Move over mustard reset position, (2) Double down on object orientation
+    
+    # (1)
+    req_position = copy.copy(mustard_position)
+    req_position[2] += 0.3
+    req_orientation = env._robot.arm.get_tip().get_orientation()
+    robot_controller.move(req_position, req_orientation, gripper_state=0)
+    
+    # (2) 
+    required_rotation = None   
+    mustard_quat = R.from_quat(list(mustard_obj.get_quaternion()))
+    desired_mustard_quat = R.from_quat(list(mustard_orientation))
+    required_rotation = mustard_quat*desired_mustard_quat.inv()
+    
+    end_effector_orintation = R.from_euler('xyz', env._robot.arm.get_tip().get_orientation())
+    new_orientation = end_effector_orintation * required_rotation
+    new_orientation = list(new_orientation.as_euler('xyz'))
+    
+    robot_controller.rotate_to(new_orientation, gripper_state=0, ignore_collisions=True)
+    
+    
+    ## Motion 6
+    # (1) Move Down. (2) Drop Object
+    
+    # (1)
+    robot_controller.translate(z=-0.3, gripper_state=0, ignore_collisions=True)
+    
+    # (2)
+    robot_controller.actuate_gripper(1)
+    
+    
+
+
+    
+    
+    
+  
+    
+
 
     
     
--- utils.py
+++ utils.py
@@ -9,7 +9,6 @@ from rlbench.tasks import *
 
 from pyrep.const import ConfigurationPathAlgorithms as Algos
 
-
 import pprint
 import time
 
@@ -111,17 +110,19 @@ class RobotController:
         self.env = env
         self.task = task
         
-    def get_pose_from_simulation(self, name):
+    def get_pose_and_object_from_simulation(self, name):
         
+        target_obj = None
         objs = self.env._scene._active_task.get_base().get_objects_in_tree(exclude_base=True, first_generation_only=False)
         for obj in objs:
             if(obj.get_name() == name):
                 req_position = obj.get_position()
                 req_orientation = obj.get_orientation()
+                target_obj = obj
                 break
             
-        return req_position, req_orientation
-    
+        return req_position, req_orientation, target_obj
+            
     def actuate_gripper(self, gripper_state=0):
 
         joint_positions = self.env._robot.arm.get_joint_positions()
@@ -129,10 +130,62 @@ class RobotController:
         for i in range(25):
             action = list(joint_positions) + [gripper_state]
             obs, reward, terminate = self.task.step(action)
-                    
+    
+            
+    
+    def translate(self, x=0, y=0, z=0, gripper_state=1, ignore_collisions=False):
+        
+        if(x == 0 and y == 0 and z == 0):
+            print("No position change requested in translate.")
+            return
+        
+        tip_position = self.env._robot.arm.get_tip().get_position()
+        tip_orientation = self.env._robot.arm.get_tip().get_orientation()
+        
+        requested_position = tip_position + np.array([x,y,z])
+        path = self.env._robot.arm.get_path(requested_position, tip_orientation, ignore_collisions=ignore_collisions)
+        path = path._path_points.reshape(-1, path._num_joints)
+                
+                
+        gripper = gripper_state
+        
+        for i in range(len(path)):
+
+            action = list(path[i]) + [gripper]
+            obs, reward, terminate = self.task.step(action)
+            
+        return path
+            
+    
+    def rotate_to(self, orientation=None, gripper_state=1, ignore_collisions=False):
+        if(orientation is None):
+            print("No rotation requested.")
+            return
+        print("Rotating")
+        tip_position = self.env._robot.arm.get_tip().get_position()
+        tip_orientation = self.env._robot.arm.get_tip().get_orientation()
+        
+        requested_orientation = orientation
+       
+        path = self.env._robot.arm.get_path(tip_position, requested_orientation, ignore_collisions=ignore_collisions, trials=1000)
+        path = path._path_points.reshape(-1, path._num_joints)
+       
+        gripper = gripper_state
+        for i in range(len(path)):
+
+            action = list(path[i]) + [gripper]
+            obs, reward, terminate = self.task.step(action)
+            
+        return path
+            
+       
+
         
         
-    def move(self, position=None, orientation=None, gripper_state=None, ignore_collisions=False):
+            
+                
+        
+    def move(self, position=None, orientation=None, gripper_state=1, ignore_collisions=False):
         
         
         if(ignore_collisions == True):
@@ -141,16 +194,11 @@ class RobotController:
             print("Planning, with collision checks")
         
         path = None
-        if(position is None and orientation is None and gripper_state is None):
-            print("No change in robot state requested.")
+        
+        if(position is None or orientation is None):
+            print("Position or orientation not specified. Check inputs.")
             return
-        elif(orientation.all()==None and gripper_state==None):
-            print("Pure translation not implemented yet.")
-            return
-        elif(position.all()==None and gripper_state==None):
-            print("Pure rotation not implemented yet.")
-            return
-        elif(gripper_state==None):
+        elif(gripper_state is None):
             print("Moving. Gripper defaults to open (1)")
             path = self.env._robot.arm.get_path(position, orientation, ignore_collisions=ignore_collisions)
             path = path._path_points.reshape(-1, path._num_joints)
@@ -159,15 +207,18 @@ class RobotController:
             path = self.env._robot.arm.get_path(position, orientation, ignore_collisions=ignore_collisions)
             path = path._path_points.reshape(-1, path._num_joints)
             
-        if(path.all() == None):
+        if(path is None):
             print("No path received..")
             return
+        
+        gripper = gripper_state
         
         ## Execute the planned path
         for i in range(len(path)):
 
-            action = list(path[i]) + [1]
+            action = list(path[i]) + [gripper]
             obs, reward, terminate = self.task.step(action)
 
+        return path
     
     
